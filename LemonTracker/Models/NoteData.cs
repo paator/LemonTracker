be2da9250764514cd4fa6f61c0f93fa6860c39f6
Kamil <paatorek@gmail.com>
2023-04-15 15:53:13 +0200

    Add pattern editor mockup

--- LemonTracker/Models/NoteData.cs
+++ LemonTracker/Models/NoteData.cs
@@ -6,4 +6,27 @@ public class NoteData
 {
     public Note Note { get; set; }
     public int Octave { get; set; }
+    
+    public override string ToString()
+    {
+        var noteString = Note switch
+        {
+            Note.C => "C-",
+            Note.Csharp => "C#",
+            Note.D => "D-",
+            Note.Dsharp => "D#",
+            Note.E => "E-",
+            Note.F => "F-",
+            Note.Fsharp => "F#",
+            Note.G => "G-",
+            Note.Gsharp => "G#",
+            Note.A => "A-",
+            Note.Asharp => "A#",
+            Note.B => "B-",
+            Note.Off => "R-",
+            _ => ".."
+        };
+        
+        return $"{noteString}{(Note == Note.None ? "." : Octave)}";
+    }
 }
