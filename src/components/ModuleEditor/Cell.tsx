9920d5fe42fec343d5b55fe25736a198a66e249c
Kamil <paatorek@gmail.com>
2023-05-26 20:05:14 +0200

    Animate cursor with pulse

--- src/components/ModuleEditor/Cell.tsx
+++ src/components/ModuleEditor/Cell.tsx
@@ -8,22 +8,29 @@ type CellProps = {
   isSelected?: boolean;
 };
 
-function Cell({ str, defaultDisplayValue, allowZero, className, isSelected }: CellProps) {
-  const isValueOrDefault = (defaultDisplayValue && str === "0" && !allowZero) || str === defaultDisplayValue;
+function Cell({
+  str,
+  defaultDisplayValue,
+  allowZero,
+  className,
+  isSelected,
+}: CellProps) {
+  const isValueOrDefault =
+    (defaultDisplayValue && str === "0" && !allowZero) ||
+    str === defaultDisplayValue;
 
   const cellClasses = classNames(
     { "text-slate-500": isValueOrDefault },
-    { "bg-blue-100 text-black": isSelected },
+    {
+      "bg-blue-100 text-black animate-[pulse_1s_ease-in-out_infinite]":
+        isSelected,
+    },
     className
   );
 
   const displayValue = isValueOrDefault ? defaultDisplayValue : str;
 
-  return (
-    <span className={cellClasses}>
-      {displayValue}
-    </span>
-  );
+  return <span className={cellClasses}>{displayValue}</span>;
 }
 
 export default Cell;
