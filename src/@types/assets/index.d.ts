736ee484e31280283d1e7b39eec4508c7e273109
Kamil <paatorek@gmail.com>
2023-05-27 19:26:38 +0200

    Add demo song at the first load

--- src/@types/assets/index.d.ts
+++ src/@types/assets/index.d.ts
@@ -1,4 +1,9 @@
-declare module "\*.svg" {
-    const svgFile: string;
-    export default svgFile;
+declare module "*.svg" {
+  const svgFile: string;
+  export default svgFile;
+}
+
+declare module "*.vt2" {
+  const vt2File: string;
+  export default vt2File;
 }
